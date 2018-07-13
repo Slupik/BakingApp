ed444a50de6eba1b2407164ae6d6e633634c9cb4
Slupik <Slupik98@gmail.com>
2018-07-13 15:07:03 +0200

    feat: Add dependeces for test libs

---
+++ app/src/androidTest/java/io/github/slupik/bakingapp/view/recipes/MainActivityTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018. by Sebastian Witasik
+ * All rights reserved. No part of this application may be reproduced or be part of other software, without the prior written permission of the publisher. For permission requests, write to the author(WitasikSebastian@gmail.com).
+ */
+
+package io.github.slupik.bakingapp.view.recipes;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+
+}
